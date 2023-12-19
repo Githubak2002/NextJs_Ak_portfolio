88651be100986aaf4972764795fe7ae049644b00
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-12-20 00:13:32 +0530

    commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,42 +9,57 @@ const About = () => {
       <h2 className="text-center text-3xl sm:text-6xl my-4 txtOutlineCSS">
         ABOUT ME
       </h2>
+      
 
-      {/* === IMG === */}
-      <div className="mx-auto w-fit ">
+      {/* ------ROW WISE------ */}
+      {/* <div className="flexCenter py-10">
+      <div className=" flexCenter w-fit md:w-1/2 mx-auto sm:px-0 px-2">
         <Image src="/imgs/abt3.png" alt="abt_png" height={310} width={360} />
       </div>
+      <h2 className="flexCenter w-auto md:w-1/2 text-center px-3 mx-auto max-w-[460px] mb-10 leading-8">I'm passionate about picking up new talents, developing them, and using them to solve the real world problems. I constantly gain the knowledge and abilities that will help me to deal with the problems in the real world. I believe that I have the capability to face the problems and find a workable solution.</h2>
+      </div> */}
+
+      <div className="" >
+      {/* === IMG === */}
+      <div className="mx-auto w-fit  sm:px-0 px-2">
+        <Image src="/imgs/abt3.png" alt="abt_png" height={310} width={360} />
+      </div>
+      <h2 className=" px-3 mx-auto max-w-[690px] mb-10 leading-10 text-xl ms:text-2xl text-justify sm:text-center">I'm passionate about picking up new talents, developing them, and using them to solve the real world problems. I constantly gain the knowledge and abilities that will help me to deal with the problems in the real world. I believe that I have the capability to face the problems and find a workable solution.</h2>
+      </div>
+
 
       {/* === EXPERIENCE === */}
-      <h2 className="txtOutlineCSS text-center text-2xl sm:text-4xl my-3">
+      <h2 className="txtOutlineCSS text-center text-2xl sm:text-5xl my-4">
         EXPERIENCE 
       </h2>
 
-      <main className="p-2 flexCenter flex-col gap-8">
+      <main className="p-2 flexCenter flex-col gap-8 text-lg sm:text-xl">
         {experience.map((ele, i) => (
           <div
             key={i}
-            className="p-2 w-full max-w-[420px] py-5 border-[1.2px] flexCenter flex-col gap-4"
+            className="p-2 w-full max-w-[560px] py-5 border-[1.2px] flexCenter flex-col gap-4"
           >
-            <h2 className="text-center font-bold text-xl  text-[#e7caff]">{ele.title}</h2>
+            <h2 className="text-xl sm:text-2xl text-center font-bold text-[#e7caff]">{ele.title}</h2>
             <h2 className="text-center ">{ele.subTitle}</h2>
             <h2 className="text-center ">{ele.details}</h2>
           </div>
         ))}
       </main>
 
+
+
       {/* === EDUCATION === */}
-      <h2 className="txtOutlineCSS text-center text-2xl sm:text-4xl my-3">
-        📖 EDUCATION 📖
+      <h2 className="txtOutlineCSS text-center text-2xl sm:text-5xl mb-10 mt-20">
+      📖 EDUCATION 📖
       </h2>
 
-      <main className="p-2 flexCenter flex-col-reverse gap-8">
+      <main className="p-2 flexCenter flex-col gap-8 text-lg sm:text-xl">
         {education.map((ele, i) => (
           <div
             key={i}
-            className="p-2 w-full max-w-[420px] py-5 border-[1.2px] flexCenter flex-col gap-4"
+            className="p-2 w-full max-w-[560px] py-5 border-[1.2px] flexCenter flex-col gap-4"
           >
-            <h2 className="font-bold text-xl  text-[#e7caff]">{ele.heading}</h2>
+            <h2 className="text-xl sm:text-2xl text-center font-bold text-[#e7caff]">{ele.heading}</h2>
             <h2 className="text-center ">{ele.detail1}</h2>
             <h2 className="text-center ">{ele.detail2}</h2>
           </div>
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -5,18 +5,18 @@ import { BiLogoGmail } from "react-icons/bi";
 import { TypeAnimation } from "react-type-animation";
 import Link from "next/link";
 
-const btncss = "mt-5 sm:mt-10 p-2 font-sans border-2 w-fit font-bold";
+const btncss = "mt-5 sm:mt-10 p-3 border hover:scale-x-125 hover:border-none transition-all  w-fit txtOutlineCSS";
 
 const HeroSection = () => {
   // const router = useRouter();
 
   return (
-    // <section className="txtOutlineCSS   ">
-    <section className="HeroFontCSS   "> 
+    // <section className="HeroFontCSS  "> 
+    <section className="txtOutlineCSS  "> 
       <main className="lg:mt-0 sm:mt-12 min-h-[80vh] px-2 sm:p-20 sm:py-0 flexCenter flex-col lg:flex-row ">
 
         <div className="lg:w-1/2 mb-6 sm:mb-8 lg:mb-0 sm:w-auto w-screen lg:text-left text-center ">
-          <h1 className="text-4xl sm:text-5xl md:text-7xl font-bold mb-3">
+          <h1 className="text-4xl sm:text-5xl md:text-7xl  mb-3">
           {/* <h1 className="font-black mb-3 text-2xl sm:text-4xl"> */}
             Hello! 👋🏼 I{" "}
             <sup>
@@ -31,7 +31,7 @@ const HeroSection = () => {
 
           {/* React text TypeAnimation */}
           <TypeAnimation
-            className="font-bold sm:text-5xl text-2xl"
+            className="sm:text-5xl text-2xl font-normal"
             sequence={[
               "Frontend WebD",
               1000,
@@ -48,13 +48,6 @@ const HeroSection = () => {
             repeat={Infinity}
           />
 
-          {/* <div className="flex lg:flex-row flex-col flexStart">
-            <Link href="https://drive.google.com/file/d/18dbHCECfPZSGMeMZ7VpLSQt4MWRQwwNV/view?usp=sharing" target="blank" className={`${btncss} lg:mr-2`}>Resume → </Link>
-            <Link href="/contact" className={`${btncss} lg:ml-2`}>
-              Contact me
-            </Link>
-          </div> */}
-
           <div className="flexCenter lg:flexStart gap-3">
             <Link href="https://drive.google.com/file/d/18dbHCECfPZSGMeMZ7VpLSQt4MWRQwwNV/view?usp=sharing" target="blank" className={`${btncss} lg:mr-2`}>Resume → </Link>
             <Link href="/contact" className={`${btncss} lg:ml-2`}>
@@ -63,16 +56,18 @@ const HeroSection = () => {
           </div>
 
           {/* --- contacts logo links → lin git mail --- */}
-          <div className="mt-10 text-4xl sm:text-6xl flexCenter gap-x-4 lg:justify-start ">
+          <div className="mt-10 text-4xl sm:text-6xl flexCenter gap-x-4 lg:justify-start font-thin ">
           <Link className="hover:scale-125 transition-all" href="https://github.com/Githubak2002" target="blank">
-              <i className="ri-github-line font-thin" />
+              <i className="ri-github-line" />
             </Link>
             <Link className="hover:scale-125 transition-all" href="https://www.linkedin.com/in/anuraglohar" target="blank">
               <i className="ri-linkedin-fill " />
             </Link>
             <Link className="hover:scale-125 transition-all" href="mailto:anuragofficial2023@gmail.com" target="blank">
+              {/* <i className="ri-mail-line  " /> */}
               <BiLogoGmail />
             </Link>
+
           </div>
         </div>
         
