bfa7c431eba35415256baaee682eeaffa7448bac
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2023-12-21 00:30:58 +0530

    commit

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
+import { contactArray } from '@/constants/constant' 
 
 const Contack = () => {
   return (
-    <section>
-      <h2 className='text-center font-black text-6xl mt-10 mb-4 txtOutlineCSS'>CONTACT</h2>
+    <section className='aboutPgFont'>
+      <h2 className='text-center text-3xl sm:text-6xl my-4 txtOutlineCSS'>CONTACT</h2>
 
       <main>
-        
+        <h2 className='text-center'>Get instant reply!</h2>
+
+
+
       </main>
 
 
